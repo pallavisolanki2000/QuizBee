828e7643690ddf81db0dfd05c6bcf401ab7bc253
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2021-05-16 00:07:16 +0530

    commit

---
+++ src/quizService/index.js
@@ -0,0 +1,375 @@
+const qBank = [
+  {
+    question:
+      "What does HTML stands for?  ",
+    answers: ["Hypertext Machine language", "Hypertext and links markup language", "Hypertext Markup Language", "Hightext machine language."],
+    correct: "	Hypertext Markup Language",
+    questionId: "099099"
+  },
+  {
+    question:
+      'Which of the following HTML Elements is used for making any text bold ?',
+    answers: ["<p>", "<i>", "<li>", "<b>"],
+    correct: "<b>",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Which of the following characters indicate closing of a tag?",
+    answers: [".", "/", "?", "!"],
+    correct: "/",
+    questionId: "267908"
+  },
+  {
+    question: "How many heading tags are there in HTML5?",
+    answers: [
+      "2",
+      "3",
+      "5",
+      "6"
+    ],
+    correct: "6",
+    questionId: "333247"
+  },
+  {
+    question: "Which of the following property is used to set the background color of an element?",
+    answers: ["background-color", "background-image", "background-repeat", "background-position"],
+    correct: "background-color",
+    questionId: "496293"
+  },
+  {
+    question:
+      "Which of the following property is used to underline, overline, and strikethrough text?",
+    answers: [
+      "text-indent",
+      "text-align",
+      "text-decoration",
+      "text-transform"
+    ],
+    correct: "text-decoration",
+    questionId: "588909"
+  },
+  {
+    question:
+      'What name represents the letter "M" in the NATO phonetic alphabet?',
+    answers: ["Mike", "Matthew", "Mark", "Max"],
+    correct: "Mike",
+    questionId: "648452"
+  },
+  {
+    question: "What is the first book of the Old Testament?",
+    answers: ["Genesis", "Exodus", "Leviticus", "Numbers"],
+    correct: "Genesis",
+    questionId: "786649"
+  },
+  {
+    question:
+      "In the video-game franchise Kingdom Hearts, the main protagonist, carries a weapon with what shape?",
+    answers: ["Key", "Sword", "Pen", "Cellphone"],
+    correct: "Key",
+    questionId: "839754"
+  },
+  {
+    question:
+      "Which best selling toy of 1983 caused hysteria, resulting in riots breaking out in stores?",
+    answers: [
+      "Cabbage Patch Kids",
+      "Transformers",
+      "Care Bears",
+      "Rubik’s Cube"
+    ],
+    correct: "Cabbage Patch Kids",
+    questionId: "98390"
+  },
+  {
+    question: "What does a funambulist walk on?",
+    answers: ["A Tight Rope", "Broken Glass", "Balls", "The Moon"],
+    correct: "A Tight Rope",
+    questionId: "1071006"
+  },
+  {
+    question: "In past times, what would a gentleman keep in his fob pocket?",
+    answers: ["Watch", "Money", "Keys", "Notebook"],
+    correct: "Watch",
+    questionId: "1174154"
+  },
+  {
+    question: "Area 51 is located in which US state?",
+    answers: ["Nevada", "Arizona", "New Mexico", "Utah"],
+    correct: "Nevada",
+    questionId: "1226535"
+  },
+  {
+    question: "How would one say goodbye in Spanish?",
+    answers: ["Adiós", " Hola", "Au Revoir", "Salir"],
+    correct: "Adiós",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the largest organ of the human body?",
+    answers: ["Skin", "Heart", "large Intestine", "Liver"],
+    correct: "Skin",
+    questionId: "1436873"
+  },
+  {
+    question: "Which sign of the zodiac is represented by the Crab?",
+    answers: ["Cancer", "Libra", "Virgo", "Sagittarius"],
+    correct: "Cancer",
+    questionId: "1515110"
+  },
+  {
+    question: "On a dartboard, what number is directly opposite No. 1?",
+    answers: ["19", "20", "12", "15"],
+    correct: "19",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "What does the 'S' stand for in the abbreviation SIM, as in SIM card? ",
+    answers: ["Subscriber", "Single", "Secure", "Solid"],
+    correct: "Subscriber",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "What word represents the letter 'T' in the NATO phonetic alphabet?",
+    answers: ["Tango", "Target", "Taxi", "Turkey"],
+    correct: "Tango",
+    questionId: "1822532"
+  },
+  {
+    question: "Which American president appears on a one dollar bill?",
+    answers: [
+      "George Washington",
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "Benjamin Franklin"
+    ],
+    correct: "George Washington",
+    questionId: "195075"
+  },
+  {
+    question: 'What is "dabbing"?',
+    answers: ["A dance", "A medical procedure", "A sport", "A language"],
+    correct: "A dance",
+    questionId: "2019778"
+  },
+  {
+    question: "What is the name of the Jewish New Year?",
+    answers: ["Rosh Hashanah", "Elul", "New Year", "Succoss"],
+    correct: "Rosh Hashanah",
+    questionId: "2134343"
+  },
+  {
+    question: "Which one of the following rhythm games was made by Harmonix?",
+    answers: [
+      "Rock Band",
+      "Meat Beat Mania",
+      "Guitar Hero Live",
+      "Dance Dance Revolution"
+    ],
+    correct: "Rock Band",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "What type of animal was Harambe, who was shot after a child fell into it's enclosure at the Cincinnati Zoo?",
+    answers: ["Gorilla", "Tiger", "Panda", "Crocodile"],
+    correct: "Gorilla",
+    questionId: "2379831"
+  },
+  {
+    question: "Red Vines is a brand of what type of candy?",
+    answers: ["Licorice", "Lollipop", "Chocolate", "Bubblegum"],
+    correct: "Licorice",
+    questionId: "2426418"
+  },
+  {
+    question: "What is the nickname of the US state of California?",
+    answers: ["Golden State", "Sunshine State", "Bay State", "Treasure State"],
+    correct: "Golden State",
+    questionId: "2510086"
+  },
+  {
+    question: "What is on display in the Madame Tussaud's museum in London?",
+    answers: [
+      "Wax sculptures",
+      "Designer clothing",
+      "Unreleased film reels",
+      "Vintage cars"
+    ],
+    correct: "Wax sculptures",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "What was the nickname given to the Hughes H-4 Hercules, a heavy transport flying boat which achieved flight in 1947?",
+    answers: ["Spruce Goose", "Noah's Ark", "Fat Man", "Trojan Horse"],
+    correct: "Spruce Goose",
+    questionId: "2796884"
+  },
+  {
+    question: "Which of these colours is NOT featured in the logo for Google?",
+    answers: ["Pink", "Yellow", "Blue", "Green"],
+    correct: "Pink",
+    questionId: "2838900"
+  },
+  {
+    question: 'What is the French word for "hat"?',
+    answers: ["Chapeau", "Bonnet", " Écharpe", " Casque"],
+    correct: "Chapeau",
+    questionId: "298060"
+  },
+  {
+    question: "Five dollars is worth how many nickles?",
+    answers: ["100", "50", "25", "69"],
+    correct: "100",
+    questionId: "3096579"
+  },
+  {
+    question: "Who is depicted on the US hundred dollar bill?",
+    answers: [
+      "Benjamin Franklin",
+      "George Washington",
+      "Abraham Lincoln",
+      "Thomas Jefferson"
+    ],
+    correct: "Benjamin Franklin",
+    questionId: "3182461"
+  },
+  {
+    question: "What do the letters in the GMT time zone stand for?",
+    answers: [
+      "Greenwich Mean Time",
+      "Global Meridian Time",
+      "General Median Time",
+      "Glasgow Man Time"
+    ],
+    correct: "Greenwich Mean Time",
+    questionId: "3239112"
+  },
+  {
+    question: "Which of the following function of Array object removes the last element from an array and returns that element?",
+    answers: ["map()", "join()", "push()", "pop()"],
+    correct: "pop()",
+    questionId: "3318503"
+  },
+  {
+    question:
+      'According to Sherlock Holmes, "If you eliminate the impossible, whatever remains, however improbable, must be the..."',
+    answers: ["Truth", "Answer", "Cause", "Source"],
+    correct: "Truth",
+    questionId: "3410327"
+  },
+  {
+    question: "Which of the following function of Array object returns a new array comprised of this array joined with other array(s) and/or value(s)?",
+    answers: ["concat()", "pop()", "push()", "some()"],
+    correct: "concat()",
+    questionId: "3542596"
+  },
+  {
+    question: "Which of the following function of Number object returns the number's value?",
+    answers: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+    correct: "valueOf()",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "Which built-in method sorts the elements of an array?",
+    answers: ["changeOrder(order)", "order()", "sort()", "None of the above."],
+    correct: "sort()",
+    questionId: "3893585"
+  },
+  {
+    question: 'Which built-in method combines the text of two strings and returns a new string?',
+    answers: ["append()", "concat()", "attach()", "None of the above."],
+    correct: "concat()",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "Which of the following algorithm cannot be desiged without recursion",
+    answers: ["Tower of Hanoi", "Fibonacci Series", "Tree Traversal", "None of the above"],
+    correct: "None of the above",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "Linked list search complexity is",
+    answers: ["O(1)", "O(n)", "O(log n)", "O(log log n)"],
+    correct: "Bearings",
+    questionId: "4235063"
+  },
+  {
+    question: "Visiting root node after visiting left and right sub-trees is called",
+    answers: ["Pre-order Traveral", "Post-order Traveral", "In-order Traversal", "none of the above"],
+    correct: "Post-order Traveral",
+    questionId: "4321002"
+  },
+  {
+    question: "What data structure is used for breadth first traversal of a graph?",
+    answers: [
+      "queue",
+      "stack",
+      "list",
+      "none of the above"
+    ],
+    correct: "queue",
+    questionId: "4442286"
+  },
+  {
+    question: "Which of the following usees FIFO method",
+    answers: [
+      "Queue",
+      "Stack",
+      "Hash Table",
+      "Binary Search Tree"
+    ],
+    correct: "Queue",
+    questionId: "4564430"
+  },
+  {
+    question: "Which of the following defines a measurement in centimeters?",
+    answers: ["ex", "em", "cm", "%"],
+    correct: "cm",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Which of the following property specifies the right padding of an element?",
+    answers: ["padding-right", "padding-left", "padding-top", "padding-bottom"],
+    correct: "padding-right",
+    questionId: "4795960"
+  },
+  {
+    question: "Which of the following property specifies the width of a border?",
+    answers: ["border-bottom-color", "border-width", "border-style", "border-color"],
+    correct: "border-width",
+    questionId: "4811162"
+  },
+  {
+    question: "Which of the following property of a anchor element signifies visited hyperlinks?",
+    answers: ["active", "hover", "visited", "link"],
+    correct: "visited",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
